477a2557a4b0347649e2d80c9d7e41e03577b663
MirQ-hub <n2vami00@students.oamk.fi>
2022-11-28 12:21:32 +0200

    lisätty collections tehtävät

--- Reima tehtävät/domNodetehtävät.js
+++ Reima tehtävät/domNodetehtävät.js
@@ -21,5 +21,26 @@ let last = document.querySelector("ul li:last-child");
 console.log(last);
 
 
+//Collections:
+//1. Print all node names of body descendants
+let all = document.querySelectorAll("body *")
+console.log(all);
+
+
+//2. Print text content of all p elements that are descendants of article element.
+let textContent = document.querySelectorAll("article p")
+console.log(textContent);
+
+
+//3. Fill all list items with default text “John Doe”
+let fill = document.querySelectorAll("li::after");
+
+
+
+
+
+
+//4. Print name of each element inside “content” that has text content longer than 10 characters
+
 
 
