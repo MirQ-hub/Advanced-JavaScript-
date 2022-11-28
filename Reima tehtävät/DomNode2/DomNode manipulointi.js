6dcc5b2e74bb2ed56e23a86b224869613f283d39
MirQ-hub <n2vami00@students.oamk.fi>
2022-11-28 13:47:49 +0200

    Tehty ensimmäiset 4 tehtävää

--- Reima tehtävät/DomNode2/DomNode manipulointi.js
+++ Reima tehtävät/DomNode2/DomNode manipulointi.js
@@ -1,7 +1,27 @@
 //1. Lisää sivun loppuun uusi hr-elementti.
-//2. Lisää hr-elementin jälkeen h3-otsikkona oma nimesi.
+//2. Lisää hr-elementin jälkeen h3-otsikkona oma 
+//nimesi.
+let content = document.querySelector("#content");
+
+let hr= document.createElement("hr");
+let h3 = document.createElement("h3");
+h3.textContent = "Mirva Väänänen"
+
+content.appendChild(hr);
+content.appendChild(h3);
+
 //3. Lisää viikonpäivien perään myös sunnuntai ”su”
+let ul = document.querySelector("ul");
+let li = document.createElement("li");
+li.textContent="Sunnuntai";
+ul.appendChild(li);
+
+//content.append(hr, h3);  <---tällä voi antaa useamman elementin jotka lisätään kerralla.
+
 //4. Poista ”turhake” lista-item.
+let turha = document.querySelector("ul li:first-child");
+
+turha.remove();
 
 
 //1. Lisää maanantai listalle. (insertbefore)
