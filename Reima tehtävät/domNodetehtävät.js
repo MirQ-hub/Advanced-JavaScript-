22f3fe9d004e60f2d226ab1fe69c49586d2e35b9
MirQ-hub <n2vami00@students.oamk.fi>
2022-11-28 12:05:43 +0200

    ensimmäiset tehtävät tehty

--- Reima tehtävät/domNodetehtävät.js
+++ Reima tehtävät/domNodetehtävät.js
@@ -1,19 +1,24 @@
 //Basic:
 //1. Print the tag name of node with id ”content”.
 let print = document.querySelector("#content");
+console.log(print);
+
+
 
 
 //2. Print the tag name of first node with class name ”basic”
-let first = document.querySelector(.basic);
+let first = document.querySelector(".basic");
+console.log(first);
 
 
 //3. Print text content of the first li-element
-let content = document.querySelector(ul.li);
-
+let content = document.querySelector("ul li");
+console.log(content);
 
 
 //4. Print text content of the last child element of ul (list)
-let last = document.querySelector()
+let last = document.querySelector("ul li:last-child");
+console.log(last);
 
 
 
