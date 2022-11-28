d844c598ccb1fe81b78824ec99ff525254c9dcb1
MirQ-hub <n2vami00@students.oamk.fi>
2022-11-28 11:16:05 +0200

    syötin tehtävät js

---
+++ Reima tehtävät/domNodetehtävät.js
@@ -0,0 +1,20 @@
+//Basic:
+//1. Print the tag name of node with id ”content”.
+let print = document.querySelector("#content");
+
+
+//2. Print the tag name of first node with class name ”basic”
+let first = document.querySelector(.basic);
+
+
+//3. Print text content of the first li-element
+let content = document.querySelector(ul.li);
+
+
+
+//4. Print text content of the last child element of ul (list)
+let last = document.querySelector()
+
+
+
+
