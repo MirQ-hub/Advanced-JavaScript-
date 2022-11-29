b2f5b8f45b912150d2beca1378eed57ccefd6bc5
MirQ-hub <n2vami00@students.oamk.fi>
2022-11-29 09:03:39 +0200

    lisätty uusi nappia ja tehtävä

---
+++ Tyylit/tyylit.js
@@ -0,0 +1,18 @@
+//Classlist
+
+//INLINE TYYLEJÄ
+
+
+let li = document.querySelector("li");
+
+li.style.color="red";
+li.style.backgroundColor="blue";
+
+let elem =  document.querySelector("ul:fist-child li");
+function changeColor{
+    for (const elem of elems)
+     {elem.classList.toggle("colorful");
+        
+    }
+}
+
