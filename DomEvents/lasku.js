36626d4ab3b88afd502d75c14c3d7ff1feacfc4d
MirQ-hub <n2vami00@students.oamk.fi>
2022-12-05 12:32:04 +0200

    haku elementti

--- DomEvents/lasku.js
+++ DomEvents/lasku.js
@@ -5,21 +5,23 @@ form.addEventListener("submit", calculate);
 
 function calculate(e){
     e.preventDefault();
+    //estää sivun automaattisen päivittymisen//
 
     let fData = new FormData(form);
     let num1 = Number(fData.get("num1"));
     let num2 = Number(fData.get("num2"));
+    let sum = num1+num2;
+
+    result.textContent = "Tulos on "+sum
 
     let arit = fData.get("arit");
 
     let res = arit === "sum" ? num1+num2 : num1-num2;
-    
-    // if(arit === "sum"){
-    //     res = num1+num2;
-    // }else{
-    //     res = num1-num2
-    // }
-
+    //on sama kuin alla oleva//
+   // if(arit === "sum"){
+     // res = num1+num2;
+    //}else{
+      //res = num1-num2;}
 
     result.textContent = "Tulos on "+ res;
 }
